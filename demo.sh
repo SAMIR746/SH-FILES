16c07768b98b382cc3a4987ed87f914c7c8c7cf1
samir746 <87543357+SAMIR746@users.noreply.github.com>
2022-01-05 14:52:04 +0600

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,12 @@
+#Project Name   - demo.sh
+#Author Name    - Md. Samir Amin
+#Author E-mail  - samiramin.cse.52@gmail.com
+#Created On     - 01/01/22, 07:12:11 PM
+
+#! /bin/sh
+echo "what is your name ?"
+read name
+echo "How old are you, $name ?"
+read age
+echo "where is your home town, $name ?"
+read home
